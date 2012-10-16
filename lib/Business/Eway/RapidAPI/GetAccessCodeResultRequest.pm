01ab5d90c1fba7ed6391fe664144cf5b074e0c26
Fayland Lam <fayland@gmail.com>
2012-10-16 15:30:45 +0800

    remove Eway so that we can add eWAY

--- lib/Business/Eway/RapidAPI/GetAccessCodeResultRequest.pm
+++
@@ -1,8 +0,0 @@
-package Business::eWAY::RapidAPI::GetAccessCodeResultRequest;
-
-use Moo;
-use MooX::Types::MooseLike::Base qw(:all);
-
-has 'AccessCode' => (is => 'rw', isa => Str);
-
-1;
--- lib/Business/Eway/RapidAPI/Payment.pm
+++
@@ -1,8 +0,0 @@
-package Business::eWAY::RapidAPI::Payment;
-
-use Moo;
-use MooX::Types::MooseLike::Base qw(:all);
-
-has $_ => (is => 'rw', isa => Str) foreach ('TotalAmount', 'InvoiceNumber', 'InvoiceDescription', 'InvoiceReference', 'CurrencyCode');
-
-1;
